e6536784b2dfd475aadb29a7baa44513178b8cc9
AVAMSHIDHAR1211 <avamsidarreddy1211@gmail.com>
2026-03-29 08:46:45 +0530

    Added Assignment3

--- assignment3.sh
+++ assignment3.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-count_lines(){
-	
-    filename="$1"
-   
-    if [ -f "$filename" ]; then
-        echo "$filename has $(wc -l < "$filename") lines"
+
+count_lines() {
+    local filename="$1"
+    if [[ -f "$filename" ]]; then
+        echo "$filename has $(wc -l < "$filename") lines."
     else
         echo "Error: $filename not found."
     fi
 }
 
+# Call with specific files
 count_lines example1.txt
 count_lines assignment1.sh
 count_lines assignment2.sh
-
-
 
